0c61320441a549d83dd82efa3e8da44ea8b40bf4
Mathieu Corre <mathieu.corre@epita.fr>
2019-04-15 09:50:29 +0200

    Merged the LEDs and the magnetometer.

--- GPS_Locator.h
+++ GPS_Locator.h
@@ -1,66 +1,16 @@
 #pragma once
 
-//#define FASTLED_FORCE_SOFTWARE_SPI
-//#define SPI_DATA
-
-//#if !defined (FASTLED_ALL_PINS_HARDWARE_SPI)
-//# pragma message "FASTLED_ALL_PINS_HARDWARE_SPI is not defined."
-//#endif
-
+//LEDs
 #include <FastLED.h>
-
-#define NB_LEDS 5
-#define MAX_BRIGHTNESS 20 //Between 0 and 255
-//#define DATA_PIN 1
-//#define CLOCK_PIN 2
+#define NB_LEDS 7
+//#define MAX_BRIGHTNESS 20 //Between 0 and 255
 CRGB leds[NB_LEDS];
 CRGB current_base[NB_LEDS];
 CRGB off_led = CRGB(0, 0, 0);
 CRGB red_led = CRGB(0, 0, 255);
 CRGB green_led = CRGB(0, 255, 0);
 CRGB blue_led = CRGB(255, 0, 0);
-int16_t brightness = MAX_BRIGHTNESS;
 
-void patternProgressBarR();
-void patternProgressBarG();
-void patternProgressBarB();
-void patternProgressBarOff();
-//void patternProgressBarFade();
-void patternPulse();
-void patternFlashSlow();
-void patternRoll();
-void patternCenter();
-void patternStatic();
-//#define NB_PATTERNS 10
-#define NB_PATTERNS 9
-static void (*patterns[NB_PATTERNS])(void) =
-{
-      patternProgressBarR,
-      patternProgressBarG,
-      patternProgressBarB,
-      patternProgressBarOff,
-      //patternProgressBarFade,
-      patternPulse,
-      patternFlashSlow,
-	  patternRoll,
-      patternCenter,
-      patternStatic
-};
-//volatile uint8_t pattern = 4; //Fade
-volatile uint8_t pattern = 6; //Roll
-volatile bool change_pattern = false;
-
-void baseRainbow();
-void baseHomogenousR();
-void baseHomogenousG();
-void baseHomogenousB();
-#define NB_BASES 4
-static void (*bases[NB_BASES])(void) =
-{
-    baseRainbow,
-	baseHomogenousR,
-	baseHomogenousG,
-	baseHomogenousB
-};
-volatile uint8_t base = -1;
-volatile bool change_base = true;
+//Magnetometer
+#include <Wire.h> //I2C Arduino Library
+#define addr 0x0D //I2C Address for The HMC5883
--- GPS_Locator.ino
+++ GPS_Locator.ino
@@ -1,26 +1,69 @@
-//#include "GPS_Locator.h"
-//#define F_CPU 16500000L
-#include <FastLED.h>
+#include "GPS_Locator.h"
 
-#define NUM_LEDS 7
+void setup()
+{
+  Serial.begin(115200);
 
-CRGB leds[NUM_LEDS];
-void setup() { FastLED.addLeds<NEOPIXEL, 1>(leds, NUM_LEDS); FastLED.setBrightness(1);}
-void loop() {
-  for (uint8_t u = 0; u < NUM_LEDS; u++)
+  FastLED.addLeds<NEOPIXEL, 2>(leds, NB_LEDS); FastLED.setBrightness(1);
+
+  Wire.begin();
+  Wire.beginTransmission(addr); //start talking
+  Wire.write(0x0B); // Tell the HMC5883 to Continuously Measure
+  Wire.write(0x01); // Set the Register
+  Wire.endTransmission();
+  Wire.beginTransmission(addr); //start talking
+  Wire.write(0x09); // Tell the HMC5883 to Continuously Measure
+  Wire.write(0x1D); // Set the Register
+  Wire.endTransmission();
+}
+
+void loop()
+{
+  for (uint8_t u = 0; u < NB_LEDS; u++)
   {
-    leds[u] = CRGB::White;
+    leds[u] = 0xFF0000;
     FastLED.show();
     delay(100);
   }
   delay(1000);
-  for (uint8_t u = 0; u < NUM_LEDS; u++)
+  for (uint8_t u = 0; u < NB_LEDS; u++)
   {
-    leds[u] = CRGB::Black;
+    leds[u] = 0x00FF00;
     FastLED.show();
     delay(100);
   }
+
+  int x, y, z; //triple axis data
+
+  //Tell the HMC what regist to begin writing data into
+
+  Wire.beginTransmission(addr);
+  Wire.write(0x00); //start with register 3.
+  Wire.endTransmission();
+
+  //Read the data.. 2 bytes for each axis.. 6 total bytes
+  Wire.requestFrom(addr, 6);
+  if (6 <= Wire.available()) {
+    x = Wire.read(); //MSB  x
+    x |= Wire.read() << 8; //LSB  x
+    z = Wire.read(); //MSB  z
+    z |= Wire.read() << 8; //LSB z
+    y = Wire.read(); //MSB y
+    y |= Wire.read() << 8; //LSB y
+  }
+
+  // Show Values
+  Serial.print("X Value: ");
+  Serial.println(x);
+  Serial.print("Y Value: ");
+  Serial.println(y);
+  Serial.print("Z Value: ");
+  Serial.println(z);
+  Serial.println();
+
+  delay(500);
 }
+
 /*
 void setup()
 {
