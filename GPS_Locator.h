627d6e07d322d8ed84fb4008a8753aa164b7fe24
Mathieu Corre <mathieu.corre@epita.fr>
2020-03-26 00:26:43 +0100

    Added support for GPS, seems to be getting no usable data, for now.

--- GPS_Locator.h
+++ GPS_Locator.h
@@ -1,7 +1,10 @@
 #pragma once
 
 //GPS
+#include <SoftwareSerial.h>
 #include <TinyGPS++.h>
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(3,4);//rx,tx
 
 //Magnetometer
 #include <Wire.h> //I2C Arduino Library
@@ -12,7 +15,7 @@
 
 #define NB_LEDS 7
 #define ANGLE_CORRECTION 160 //in degrees, to compensate for 
-//#define MAX_BRIGHTNESS 20 //Between 0 and 255
+#define BRIGHTNESS 1 //Between 0 and 255
 CRGB leds[NB_LEDS];
 CRGB current_base[NB_LEDS];
 CRGB off_led = CRGB(0, 0, 0);
--- GPS_Locator.ino
+++ GPS_Locator.ino
@@ -1,42 +1,40 @@
 #include "GPS_Locator.h"
 
-TinyGPSPlus gps;
-
 void rainbowCircle()
 {
-  uint8_t coeff = 0xff * 6 / NB_LEDS;
+  uint8_t coeff = 0xff * 6 / (NB_LEDS - 1);
   static uint8_t u;
   static uint8_t tmp;
-  for (u = 0; u < (NB_LEDS / 6); u++) {
+  for (u = 0; u < ((NB_LEDS - 1) / 6); u++) {
       //0 - nb/6:            R: 255; G: 0 - 255; B: 0
       leds[u] = CRGB(255, u * coeff, 0);
   }
   tmp = 0;
-  for (; u < (NB_LEDS / 3); u++) {
+  for (; u < ((NB_LEDS - 1) / 3); u++) {
       //nb/6 - 2nb/6:     R: 255 - 0; G: 255; B: 0
       leds[u] = CRGB(255 - tmp * coeff, 255, 0);
       tmp++;
   }
   tmp = 0;
-  for (; u < (NB_LEDS / 2); u++) {
+  for (; u < ((NB_LEDS - 1) / 2); u++) {
       //2nb/6 - 3nb/6:    R: 0; G: 255; B: 0 - 255
       leds[u] = CRGB(0, 255, tmp * coeff);
       tmp++;
   }
   tmp = 0;
-  for (; u < (2 * NB_LEDS / 3); u++) {
+  for (; u < (2 * (NB_LEDS - 1) / 3); u++) {
       //3nb/6 - 4nb/6:    R: 0; G: 255 - 0; B: 255
       leds[u] = CRGB(0, 255 - tmp * coeff, 255);
       tmp++;
   }
   tmp = 0;
-  for (; u < (5 * NB_LEDS / 6); u++) {
+  for (; u < (5 * (NB_LEDS - 1) / 6); u++) {
       //4nb/6 - 5nb/6:    R: 0 - 255; G: 0; B: 255
       leds[u] = CRGB(tmp * coeff, 0, 255);
       tmp++;
   }
   tmp = 0;
-  for (; u < NB_LEDS; u++) {
+  for (; u < (NB_LEDS - 1); u++) {
       //5nb/6 - nb:       R: 255; G: 0; B: 255 - 0
       leds[u] = CRGB(255, 0, 255 - tmp * coeff);
       tmp++;
@@ -47,17 +45,58 @@ void rainbowCircle()
     static CRGB *p;
     static uint8_t index;
     tmp = leds[NB_LEDS - 1];
-    for (index = NB_LEDS - 1; index > 0; index--) {
+    for (index = NB_LEDS - 1; index > 1; index--) {
         p = leds + index - 1;
         leds[index] = *p;
     }
-    leds[0] = tmp;
-    delay(10);
+    leds[1] = tmp;
+    FastLED.show();
+    delay(200);
   }
 }
 
 bool satelitesAcquired()
 {
+  while (gpsSerial.available() > 0)
+  {
+    //delay(1);
+    auto read = gpsSerial.read();
+    gps.encode(read);
+    Serial.print("Got data from the GPS: '");
+    Serial.print(read);
+    Serial.println("'");
+  Serial.println(gps.location.lat(), 6); // Latitude in degrees (double)
+  Serial.println(gps.location.lng(), 6); // Longitude in degrees (double)
+  //Serial.print(gps.location.rawLat().negative ? "-" : "+");
+  //Serial.println(gps.location.rawLat().deg); // Raw latitude in whole degrees
+  //Serial.println(gps.location.rawLat().billionths);// ... and billionths (u16/u32)
+  //Serial.print(gps.location.rawLng().negative ? "-" : "+");
+  //Serial.println(gps.location.rawLng().deg); // Raw longitude in whole degrees
+  //Serial.println(gps.location.rawLng().billionths);// ... and billionths (u16/u32)
+  //Serial.println(gps.date.value()); // Raw date in DDMMYY format (u32)
+  //Serial.println(gps.date.year()); // Year (2000+) (u16)
+  //Serial.println(gps.date.month()); // Month (1-12) (u8)
+  //Serial.println(gps.date.day()); // Day (1-31) (u8)
+  //Serial.println(gps.time.value()); // Raw time in HHMMSSCC format (u32)
+  //Serial.println(gps.time.hour()); // Hour (0-23) (u8)
+  //Serial.println(gps.time.minute()); // Minute (0-59) (u8)
+  //Serial.println(gps.time.second()); // Second (0-59) (u8)
+  //Serial.println(gps.time.centisecond()); // 100ths of a second (0-99) (u8)
+  //Serial.println(gps.speed.value()); // Raw speed in 100ths of a knot (i32)
+  //Serial.println(gps.speed.knots()); // Speed in knots (double)
+  //Serial.println(gps.speed.mph()); // Speed in miles per hour (double)
+  //Serial.println(gps.speed.mps()); // Speed in meters per second (double)
+  //Serial.println(gps.speed.kmph()); // Speed in kilometers per hour (double)
+  //Serial.println(gps.course.value()); // Raw course in 100ths of a degree (i32)
+  //Serial.println(gps.course.deg()); // Course in degrees (double)
+  //Serial.println(gps.altitude.value()); // Raw altitude in centimeters (i32)
+  //Serial.println(gps.altitude.meters()); // Altitude in meters (double)
+  //Serial.println(gps.altitude.miles()); // Altitude in miles (double)
+  //Serial.println(gps.altitude.kilometers()); // Altitude in kilometers (double)
+  //Serial.println(gps.altitude.feet()); // Altitude in feet (double)
+  //Serial.println(gps.satellites.value()); // Number of satellites in use (u32)
+  //Serial.println(gps.hdop.value()); // Horizontal Dim. of Precision (100ths-i32)
+  }
   return gps.location.isUpdated();
 }
 
@@ -88,9 +127,11 @@ void showNorth(uint16_t heading)
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("========== RESET ==========");
 
-  FastLED.addLeds<NEOPIXEL, 2>(leds, NB_LEDS); FastLED.setBrightness(1);
-
+  FastLED.addLeds<NEOPIXEL, 2>(leds, NB_LEDS); FastLED.setBrightness(BRIGHTNESS);
+  for (uint8_t u = 0; u < NB_LEDS; u++)
+    leds[u] = red_led;
   Wire.begin();
   Wire.beginTransmission(addr); //start talking
   Wire.write(0x0B); // Tell the HMC5883 to Continuously Measure
@@ -102,10 +143,16 @@ void setup()
   Wire.endTransmission();
 
   //Initialize GPS
+  gpsSerial.begin(9600); // connect gps sensor
 
   //Wait for the GPS to acquire satelites
-  while (satelitesAcquired())
+  while (!satelitesAcquired())
     rainbowCircle();
+  Serial.println("Acquired a position!");
+  for (uint8_t u = 0; u < NB_LEDS; u++)
+    leds[u] = green_led;
+  FastLED.show();
+  delay(20000);
 }
 
 void loop()
